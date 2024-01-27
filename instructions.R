2b754ba7c3a8e15cd64d7530ac0368b162bfa110
Maximiliano Rodriguez <xam.rodriguez@gmail.com>
2024-01-27 12:05:55 -0300

    Initial instructions to set the connection between R and git with the file in my USB stick

---
+++ instructions.R
@@ -0,0 +1,13 @@
+library(usethis)
+use_git()
+
+#Instructions to make the gib tab appear
+# In command Prompt
+# First: I went to the folder with problem
+# Run: git status
+# If problems "fatal"
+# Then I run: git config --global --add safe.directory M:/10_Practice3/TTT3
+# Then git status. If ok then safe
+# git add .gitignore TTT3.Rproj
+#Then commit the changes with: git commit -m "message"
+#Then reopen project in R and it should work!, git tab should be visible
