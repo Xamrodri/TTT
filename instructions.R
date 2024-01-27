5292303d8d35364380db56966279af89296a01fe
Maximiliano Rodriguez <xam.rodriguez@gmail.com>
2024-01-27 12:37:35 -0300

    Added head at the file

--- instructions.R
+++ instructions.R
@@ -1,7 +1,15 @@
+# Script with instructions to establish the connection between R and git
+#
+# copyright (c) 2024 - House
+# written by: Maximiliano Rodriguez
+#
+# last modified Jan, 2024
+# first written Jan, 2024
+
 library(usethis)
 use_git()
 
-#Instructions to make the gib tab appear
+# Instructions to make the gib tab appear
 # In command Prompt
 # First: I went to the folder with problem
 # Run: git status
@@ -9,7 +17,7 @@ use_git()
 # Then I run: git config --global --add safe.directory M:/10_Practice3/TTT3
 # Then git status. If ok then safe
 # git add .gitignore TTT3.Rproj
-#Then commit the changes with: git commit -m "message"
-#Then reopen project in R and it should work!, git tab should be visible
+# Then commit the changes with: git commit -m "message"
+# Then reopen project in R and it should work!, git tab should be visible
 
 #New instruction
